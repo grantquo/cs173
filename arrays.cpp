989b4e007361eadf5551a107b6e6c8ebf39b4da1
grantquo <grantgutterman@gmail.com>
2022-09-14 12:21:40 -0400

    end of class 09-14-2022

--- arrays.cpp
+++ arrays.cpp
@@ -5,17 +5,54 @@
 // Random file practicing arrays.
 //====================
 
-
+#define SIZE 5
 #include <iostream>
-#include <string>
+#include <iomanip>
 using namespace std;
+
+//function declarations
+void printArray ( float a[], int n );
 
 //====================
 // main
 //====================
 int main ( void )
 {
-    
+    float data[SIZE];
+
+    // prompt to fill
+
+    for (int i=0; i<SIZE; i++)
+    {
+        cout << "Enter value " << i+1 << ": ";
+        cin >> data[i];
+    }
+
+    // output by index
+
+    for (int i=0; i<SIZE; i++)
+    {
+        cout << "Index " << i << ": " << fixed << setw(6) << setprecision(2)
+        << data[i] << endl;
+    }
 
     return 0;
+}
+
+//====================
+// printArray
+// PARAMETERS:
+// - a : an array of floats
+// - n : integer size of array
+// RETURN:
+// - void
+//====================
+
+void printArray ( float a[], int n )
+{
+    for (int i=0; i<SIZE; i++){
+        cout << "Value " << i+1 << " = " << fixed << setw(6)
+        << setprecision(2) << a[i] << endl;
+    }
+
 }
