296c6e5c955a747522c19d4c81d46d80b654736f
grantquo <grantgutterman@gmail.com>
2022-09-16 13:10:11 -0400

    completed text input. building main loop

--- project3/project3.cpp
+++ project3/project3.cpp
@@ -15,7 +15,7 @@
 using namespace std;
 
 // function declarations
-int integerize();
+int integerize(char character);
 
 char convert();
 
@@ -24,39 +24,41 @@ char convert();
 // ============================================
 int main ( int argc, char *argv[] )
 {
+
     char alpha[SIZE] = {('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l'
     , 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z')};
 
     int values[SIZE] = {(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16,
     17, 18, 19, 20, 21, 22, 23, 24, 25)};
 
+    // FILE INPUTS
+
     int key;
     if ( argc > 1 )
         key = atoi(argv[1]);
     else
         key = 3;
-    string text;    
-    cin >> text;
-    cout << text << endl;
-    cout << key << endl;
+
+    string message, temp;
+
+    while ( getline(cin,temp) )
+        message = message+temp;
 
 
-
-
-
-
-
-
-
-
-
-
+    /*
+    for (int i=0; i<message.length(); i++)
+    {
+        if ( isspace(message[i]) == 0 ){
+            cout << message[i];
+        }
+    }
+    */
 
     return 0;
 }
 
 
-int integerize(){
+int integerize(char character){
 
     return 0;
 }
