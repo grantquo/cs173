f2357b2cf9463dda24689348663a26873d7b01dc
grantquo <grantgutterman@gmail.com>
2022-09-15 20:44:01 -0400

    slowly drawing things out

--- project3/project3.cpp
+++ project3/project3.cpp
@@ -6,12 +6,23 @@
 // decrypted message.
 
 #include <iostream>
+#include <iomanip>
 #include <string>
+#include <cctype>
+
+int integerize(text);
+
+char convert(value, key);
+
 
 using namespace std;
 
 int main ( void )
 {
+    char alpha[25] = ('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l'
+    , 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z')
+
+    
 
 
 
@@ -27,7 +38,16 @@ int main ( void )
 
 
 
+    return 0;
+}
 
+
+integerize(text, key){
+
+    return 0;
+}
+
+convert(value, key){
 
     return 0;
 }
