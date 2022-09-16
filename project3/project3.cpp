e330c887b088e52255ed9a66c639b13326465ad4
grantquo <grantgutterman@gmail.com>
2022-09-15 22:12:03 -0400

    adding test files. still figuring out how tf to input them in

--- project3/project3.cpp
+++ project3/project3.cpp
@@ -4,27 +4,41 @@
 // project3.cpp
 // Ceaser Cypher program that decrypts by taking a cipher text and returns a
 // decrypted message.
+// ============================================
 
+#define SIZE 25
 #include <iostream>
 #include <iomanip>
-#include <string>
 #include <cctype>
-
-int integerize(text);
-
-char convert(value, key);
-
-
+#include <string>
+#include <fstream>
 using namespace std;
 
-int main ( void )
+// function declarations
+int integerize();
+
+char convert();
+
+// ============================================
+// main
+// ============================================
+int main ( int argc, char *argv[] )
 {
-    char alpha[25] = ('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l'
-    , 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z')
+    char alpha[SIZE] = {('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l'
+    , 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z')};
 
-    
+    int values[SIZE] = {(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16,
+    17, 18, 19, 20, 21, 22, 23, 24, 25)};
 
-
+    int key;
+    if ( argc > 1 )
+        key = atoi(argv[1]);
+    else
+        key = 3;
+    string text;    
+    cin >> text;
+    cout << text << endl;
+    cout << key << endl;
 
 
 
@@ -42,12 +56,12 @@ int main ( void )
 }
 
 
-integerize(text, key){
+int integerize(){
 
     return 0;
 }
 
-convert(value, key){
+char convert(){
 
     return 0;
 }
