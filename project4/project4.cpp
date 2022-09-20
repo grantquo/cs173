743e7ce6cdf74c2de217c6c203832c93e5fc766d
grantquo <grantgutterman@gmail.com>
2022-09-20 11:50:16 -0400

    main while loop fixed

--- project4/project4.cpp
+++ project4/project4.cpp
@@ -17,28 +17,23 @@ int main ( void )
     int year;
     while (year != 0)
     {
-
-        cout << "Enter a year from 1 to 2022 (0 to quit): " << endl;
+        cout << "Enter a year from 1 to 2022 (0 to quit): ";
         cin >> year;
-        if year < 1 || year > 2022
+        if (year < 0 || year > 2022)
         {
-            cout << "Error! Year out of range. Try again."
-            continue
+            cout << "Error! Year out of range. Try again." << endl;
+            continue;
         }
-
-
-
+        /*
+        string output = convert(year);
+        cout << year << " = " << output << endl;
+        */
     }
 
-
-
-
-
-
-    return 0
+    return 0;
 }
 
 string convert(int year){
 
-    return 0
+    return 0;
 }
