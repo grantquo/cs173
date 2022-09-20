eb02accd600afc197b83bb4dc845b036b8a288b6
grantquo <grantgutterman@gmail.com>
2022-09-20 11:30:25 -0400

    project4 first commit

---
+++ project4/project4.cpp
@@ -0,0 +1,44 @@
+//==========================================================
+// Grant Gutterman
+// 09-23-2022
+// project4.cpp
+// Description
+//==========================================================
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+string convert(int year);
+
+int main ( void )
+{
+
+    int year;
+    while (year != 0)
+    {
+
+        cout << "Enter a year from 1 to 2022 (0 to quit): " << endl;
+        cin >> year;
+        if year < 1 || year > 2022
+        {
+            cout << "Error! Year out of range. Try again."
+            continue
+        }
+
+
+
+    }
+
+
+
+
+
+
+    return 0
+}
+
+string convert(int year){
+
+    return 0
+}
