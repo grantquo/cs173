c076e83ae67f2ee2da653fe47dca2cf2e2ac22ca
grantquo <grantgutterman@gmail.com>
2022-09-30 18:29:41 -0400

    debugging draft search function

--- project4/project4.cpp
+++ project4/project4.cpp
@@ -1,45 +1,65 @@
-//==========================================================
-// Grant Gutterman
-// 09-23-2022
-// project4.cpp
-// Description
-//==========================================================
+//==============================================================
+//Zheng and Grant
+//September 2022
+//project4.cpp
+//translate decimal values into Roman numerals
+//==============================================================
 
 #include <iostream>
 #include <string>
 using namespace std;
+//==============================================================
+//convert
+//This function convert the decimal number to Roman numerals
+// PARAMETERS:
+// - year  : a decimal integer between 1 and 2022
+// RETURN :  
+// - result : the translsted Roman numeral of year
+//==============================================================
 
-int convert(int year);
+string convert (int year)
+{
+	string roman_numerals[] = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+        int decimal_values[] = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+
+        string result = "";
+
+        for (int i = 0; i < 13; ++i)
+        {
+            	while(year - decimal_values[i] >= 0)
+            	{
+                	result += roman_numerals[i];
+                	year -= decimal_values[i];
+            	}
+        }
+	return result;
+}
+
+//==================
+//main function
+//===================
 
 int main ( void )
 {
-
-    int year;
-    while (year != 0)
-    {
-        cout << "Enter a year from 1 to 2022 (0 to quit): ";
-        cin >> year;
-        if (year < 0 || year > 2022)
-        {
-            cout << "Error! Year out of range. Try again." << endl;
-            continue;
-        }
-
-        int output = convert(year);
-        cout << year << " = " << output << endl;
-
-    }
-
-    return 0;
-}
-
-int convert(int year){
-    int ones, tens, hundreds, thousands;
-    ones = (year)%10;
-    tens = (year-ones)%100;
-    hundreds = (year-tens-ones)%1000;
-    thousands = (year-hundreds-tens-ones)%10000;
-    // cout << ones << ", " << tens << ", " << hundreds << ", " << thousands << endl;
-    
-
+	
+	int year;
+	
+	while (year != 0)
+	{
+		cout << "Enter year 1 to 2022 (0 to quit): " << endl;
+		cin  >> year;
+		if ( year < 0 || year > 2022)
+		{
+			cout << "Invalid Entry. Try again." << endl;
+			continue;
+		}
+		else if ( year == 0)
+		{
+			return 0;
+		}
+		cout << year << "=" << convert(year) << endl;
+	}
+	
+	return 0;
+	
 }
--- project5/project5.cpp
+++ project5/project5.cpp
@@ -7,24 +7,51 @@
 
 // packages
 #include <iostream>
+#include <fstream>
+#include <cstring>
 
 using namespace std;
 
-const int MAX = 20;
+int MAX = 100;
 
-int strandSearch(char dna, char fragment);
+int strandSearch(char maindna, char segment1);
 
 int main ( void ){
 
-    char dna[MAX];
     // inputs
-    cin >> "ATCGATTGAGGAC";
+    char dna[MAX] = {'A','G','T','C','C','G','A','T','C','C','G','A','\0'};
+    cin >> dna;
+    int searchNum;
+    int indexNum;
+    // for (int i=0;i<searchNum;i++)
+    //     char segment[MAX];
+    //     cin >> segment;
+    //     indexNum = strandSearch(char dna, char segment);
+    //     cout << "The index of " << segment << " is " << indexNum << "." << endl;
+    char segment[MAX] = {'G','A','T','C','C','\0'};
+    indexNum = strandSearch(dna, segment);
 
-    cout << dna;
 
     return 0;
 }
 
-int strandSearch(char dna, char fragment){
+int strandSearch(char maindna, char segment1){
+    int dnaLen = strlen(dna);
+    int segLen = strlen(segment);
+    int dnaIndex = 0;
+    while (dnaIndex<dnaLen-segLen){
+        cout << dnaIndex << endl;
+        char dnaSlice[segLen+1];
+        for (int segIndex=0; segIndex<segLen; segIndex++){
+            int workingIndex = dnaIndex+segIndex;
+            dnaSlice[segIndex] = dna[workingIndex];
+            cout << dnaSlice << endl;
+        int condOutput = strcmp(segment, dnaSlice);
+        if (condOutput == 0)
+            return dnaIndex;
+        else
+            dnaIndex++;
+        }
+    }
     return 0;
 }
