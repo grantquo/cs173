916e7426db26bda4df4f0c205e6acdc1d4ac05df
grantquo <grantgutterman@gmail.com>
2022-09-30 15:52:16 -0400

    figuring out wtf to do for the input of this program

---
+++ project5/project5.cpp
@@ -0,0 +1,30 @@
+// ===========================================
+// Grant Gutterman
+// 09-30-22
+// project5.cpp
+// Long ass project description
+// ===========================================
+
+// packages
+#include <iostream>
+
+using namespace std;
+
+const int MAX = 20;
+
+int strandSearch(char dna, char fragment);
+
+int main ( void ){
+
+    char dna[MAX];
+    // inputs
+    cin >> "ATCGATTGAGGAC";
+
+    cout << dna;
+
+    return 0;
+}
+
+int strandSearch(char dna, char fragment){
+    return 0;
+}
