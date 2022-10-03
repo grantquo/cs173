17a5cdce0a99c9bfd190e7376937303523c5178a
grantquo <grantgutterman@gmail.com>
2022-10-03 12:19:38 -0400

    beginning project 6 / end of class 10-03

---
+++ project6/project6.cpp
@@ -0,0 +1,29 @@
+// =======================================
+// Grant and Jack
+// project6.cpp
+// Project Description
+// =======================================
+
+
+#include <iostream>
+#include <ifstream>
+
+using namespace std;
+
+// function declarations
+void printGrid ( char grid[length][length] );
+
+const int length = 14;
+
+int main(  void  ){
+
+
+
+
+    return 0;
+}
+
+
+void printGrid ( char grid[length][length]){
+    return;
+}
