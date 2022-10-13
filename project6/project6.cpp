fe11c6ece83fa7d1dda60cd4d52312b7fbb8fc98
grantquo <grantgutterman@gmail.com>
2022-10-12 21:35:31 -0400

    building recursive continuity and ensuring memory / if statements built

--- project6/project6.cpp
+++ project6/project6.cpp
@@ -17,6 +17,7 @@ void readGrid( char grid[LENGTH][LENGTH] );
 void printGrid ( char grid[LENGTH][LENGTH] );
 void findWord ( char grid[LENGTH][LENGTH], char source, char target, int r, int c );
 //bool isConnected( char grid[LENGTH][LENGTH], char source, char target, int r, int c);
+void printLocation (char target, int r, int c);
 
 // =======================================
 
@@ -41,9 +42,9 @@ int main(  void  ){
         cin >> word;
         cout << word << endl;
 // searching
-        findWord(grid, grid[0][0], word[1]);
+        string temp = "";
+        findWord(grid, word, temp, grid[0][0], word[0], 0, 0);
     }
-
     return 0;
 }
 // =======================================
@@ -67,14 +68,48 @@ void printGrid ( char grid[LENGTH][LENGTH] ){
     }
     return;
 }
+// =======================================
 
-void findWord ( char grid[LENGTH][LENGTH], char source, char target, int r, int c ){
+// =======================================
+void findWord ( char grid[LENGTH][LENGTH], char word, char temp, char source, char target, int r, int c ){
     assert(r>=0);
     assert(c>=0);
     assert(r<LENGTH);
     assert(c<LENGTH);
 
+    if (word == temp) source
+        return printLocation(word, r, c);
+    // string temp = "";
+    if (source == target)
+        temp += source;
 
+    if (r>0)
+        findWord(grid, word, temp, source, target, r, c)
+    if (r<LENGTH-1)
+        // check down
+    if (c>0)
+        // check left
+    if (c<LENGTH-1)
+        // check right
+    if (r>0 && c>0)
+        // check up/left
+    if (r>0 && c<LENGTH-1)
+        // check up/right
+    if (r<LENGTH-1 && c>0)
+        // check down/left
+    if (r<LENGTH-1 && c<LENGTH-1)
+        // check down/right
 
+    return printLocation(target, -1. -1);
+}
+// =======================================
+
+// =======================================
+void printLocation( char target, int r, int c ){
+    /*
+    if r & c < 0:
+        cout << "The word "<< target <<" was not found in the puzzle." << endl;
+    */
+    // cout << target " found at " << r << "," << c << endl;
     return;
 }
