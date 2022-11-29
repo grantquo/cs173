e9e88a56c749172fbd85af69268ad39d3809a881
grantquo <grantgutterman@gmail.com>
2022-11-29 12:01:08 -0500

    beginning project12

---
+++ project12/project12.cpp
@@ -0,0 +1,36 @@
+//=====================================================
+// Grant Gutterman
+// 12-9-22
+// project12.cpp
+// Stack program that tracks parens and ensures they
+// are balanced.
+//=====================================================
+
+// inclusions and function declarations
+
+#include <iostream>
+#include <string>
+#include <stack>
+
+void errorReport(int type, int lineNum, int charNum);
+
+using namespace std;
+
+int main ( void )
+{
+
+    // read in file
+
+    stack<string> s;
+
+
+
+    return 0;
+}
+
+// function definitions
+
+void errorReport(int type, int lineNum, int charNum)
+{
+    return;
+}
