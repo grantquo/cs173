7588b25f98475341b1320d1b958af91a4bc98c62
grantquo <grantgutterman@gmail.com>
2022-12-09 14:51:30 -0500

    project 12 finished

--- project12/project12.cpp
+++ project12/project12.cpp
@@ -1,10 +1,10 @@
-//=====================================================
-// Grant Gutterman
+//=============================================================
+// Grant Gutterman, Cong Nguyen, & Di Wu
 // 12-9-22
 // project12.cpp
 // Stack program that tracks parens and ensures they
 // are balanced.
-//=====================================================
+//=============================================================
 
 // inclusions and function declarations
 
@@ -13,6 +13,7 @@
 #include <stack>
 using namespace std;
 
+// 'Loc' datatype.
 struct Loc
 {
     int lineNum;
@@ -20,13 +21,24 @@ struct Loc
 };
 
 string lineReader( void );
-void paremBalance(stack<char> s, stack<Loc> l, string curLine, int curLineNum);
+void parenBalance(stack<char> s, stack<Loc> l, string curLine, int curLineNum);
 bool comparator(char s1, char s2);
 void clearStacks(stack<char> s, stack<Loc> l);
-void errorReport(char openParem, char closedParem, int type, int lineNum, int charNum);
+void errorReport(char openParen, char closedParen, int type, int lineNum, int charNum);
 
 
 
+//=============================================================
+// main
+// This is the main function of the program. It's purpose is to
+// initialize a locator and char stack, while reading line by
+// line the file. This sends the string lines to parenBalance to
+// check for errors. It also keeps track of the line number index.
+// PARAMS:
+//      none
+// RETURNS:
+//      nothing
+//=============================================================
 int main ( void )
 {
     stack<char> s;
@@ -36,14 +48,23 @@ int main ( void )
     string curLine = lineReader();
     while (curLine != "")
     {
-        paremBalance(s, l, curLine, curLineNum);
-        curLineNum++; // +1 line index
-        curLine = lineReader(); // go to next line
+        parenBalance(s, l, curLine, curLineNum);
+        curLineNum++;
+        curLine = lineReader();
     }
 
     return 0;
 }
 
+//=============================================================
+// lineReader
+// Simple function for using stdin to read a user-given file line
+// by line.
+// PARAMS:
+//      none
+// RETURNS:
+//      A string of a full line from a text file.
+//=============================================================
 string lineReader( void )
 {
     string input;
@@ -51,12 +72,27 @@ string lineReader( void )
     return input;
 }
 
-void paremBalance(stack<char> s, stack<Loc> l, string curLine, int curLineNum)
+//=============================================================
+// parenBalance
+// This function completes the main error tracking of the program.
+// It loops through every character from a line in the file and
+// checks for paren imbalances, calling the error reporting function
+// when it finds one.
+// PARAMS:
+//      stack<char> s: A stack that hold open parens.
+//      stack<Loc> l: This stack holds a struct that contains
+//      character and line number information for each open parens.
+//      string curLine: String of a line of text read from lineReader.
+//      int curLineNum: Integer for index line number.
+// RETURNS:
+///     Nothing, but calls errorReport to output error information.
+//=============================================================
+void parenBalance(stack<char> s, stack<Loc> l, string curLine, int curLineNum)
 {
     for (int curCharNum = 0; curCharNum <= curLine.length(); curCharNum++) // loop through line
     {
-        char curChar = curLine[curCharNum]; // obtain character from line string
-        // if an open parem
+        char curChar = curLine[curCharNum];
+
         if (curChar == '(' || curChar == '[' || curChar == '{')
         {
             s.push(curChar);
@@ -66,46 +102,42 @@ void paremBalance(stack<char> s, stack<Loc> l, string curLine, int curLineNum)
             l.push(curLoc);
         }
 
-        // if a closed parem
         else if (curChar == ')' || curChar == ']' || curChar == '}')
         {
-            if (s.empty() == false) // if stack isn't empty
+            if (s.empty() == false)
             {
-                // assign open parem
-                // remove open parem from stack
                 char stackTop = s.top();
                 s.pop();
 
-                // assign open parem locs
-                // remove open parem locs from stack
                 Loc topLoc = l.top();
                 int openLineNum = topLoc.lineNum;
                 int openCharNum = topLoc.charNum;
                 l.pop();
 
-                // if no match, ERROR 1 !!!!!!!!!!!
                 if (comparator(stackTop, curChar) == false)
                 {
                     errorReport(stackTop, curChar, 1, curLineNum, curCharNum);
-                    // finishing output with error locs data
                     cout << " found at line " << openLineNum+1 << " char "
                     << openCharNum+1 << " ." << endl;
                 }
             }
-
             else if (s.empty() == true)
-            {
-                // if stack is empty
-                // report Type ERROR 3 !!!!!!!!!!!!!
                 errorReport('O', curChar, 3, curLineNum, curCharNum);
-            }
-
         }
-
     }
     clearStacks(s, l);
 }
 
+//=============================================================
+// comparator
+// Simple comparing function that takes an open and closed parens
+// and see if they are matching.
+// PARAMS:
+//      char open: An open paren in a char type.
+//      char closed: A closed paren in a char type.
+// RETURNS:
+//      A Boolean true or false for if they are matching.
+//=============================================================
 bool comparator(char open, char closed)
 {
     if (open == '(' && closed == ')')
@@ -117,37 +149,62 @@ bool comparator(char open, char closed)
     return false;
 }
 
+//=============================================================
+// clearStacks
+// This function checks for any leftover data in the location
+// and open paren stacks, and then send their information for
+// error Type II reporting.
+// PARAMS:
+//      stack<char> s: A stack that hold open parens.
+//      stack<Loc> l: This stack holds a struct that contains
+//      character and line number information for each open parens.
+// RETURNS:
+//      Nothing, but calls errorReporting that outputs through
+//      stdout.
+//=============================================================
 void clearStacks(stack<char> s, stack<Loc> l)
 {
     while (s.empty() == false && l.empty() == false)
     {
-        char topOpenParem = s.top();
+        char topOpenParen = s.top();
         Loc topLoc = l.top();
         int topLineNum = topLoc.lineNum;
         int topCharNum = topLoc.charNum;
-        errorReport(topOpenParem, 'O', 2, topLineNum, topCharNum);
+        errorReport(topOpenParen, 'O', 2, topLineNum, topCharNum);
         s.pop();
         l.pop();
     }
 }
-//
-void errorReport(char openParem, char closedParem, int type, int lineNum, int charNum)
+
+//=============================================================
+// errorReport
+// This function takes in information and uses them to report
+// errors through stdout.
+// PARAMS:
+//      char openParen: An open paren.
+//      char closedParen: A closed paren.
+//      int type: An integer that denotes what type of error.
+//      int lineNum: Index number for the line in the text file.
+//      int charNum: Index number for the char in the text file.
+// RETURNS:
+//      Nothing is returned, but stdout outputs error information
+//      and location.
+//=============================================================
+void errorReport(char openParen, char closedParen, int type, int lineNum, int charNum)
 {
     if (type == 1)
     {
-        cout << "Type I Error: " << closedParem << " found at line " << lineNum+1
-        << " char " << charNum+1 << " does not match " << openParem;
+        cout << "Type I Error: " << closedParen << " found at line " << lineNum+1
+        << " char " << charNum+1 << " does not match " << openParen;
     }
-
     else if (type == 2)
     {
-        cout << "Type II Error: " << openParem << " found at line " << lineNum+1
+        cout << "Type II Error: " << openParen << " found at line " << lineNum+1
         << " char " << charNum+1 << " with no matching close paren." << endl;
     }
-
     else
     {
-        cout << "Type III Error: " << closedParem << " found at line " << lineNum+1
+        cout << "Type III Error: " << closedParen << " found at line " << lineNum+1
         << " char " << charNum+1 << " with no matching open paren." << endl;
     }
 }
